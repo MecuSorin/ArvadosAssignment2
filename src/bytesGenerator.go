3e584cb5317fc44847ab7c2158612fc035d78de6
Mecu Mihai Sorin <mecusorin@gmail.com>
2016-08-07 13:42:36 +0300

    Added bytes generation utility

--- src/bytesGenerator.go
+++ src/bytesGenerator.go
@@ -10,6 +10,23 @@ import (
 // MIB is a standard measure
 const MIB = 1048576
 
+func getBlob(size int) (io.Reader, error) {
+	if nil != validateBlobSize(size) {
+		return nil, errors.New("Invalid size for the blob")
+	}
+
+	pipeReader, pipeWriter := io.Pipe()
+	wb64 := base64.NewEncoder(base64.StdEncoding, pipeWriter)
+	go func() {
+		defer pipeWriter.Close()
+		defer wb64.Close()
+
+		for i := 0; i < size; i++ {
+			wb64.Write([]byte{byte(rand.Intn(256))})
+		}
+	}()
+	return pipeReader, nil
+}
 
 func validateBlobSize(size int) error {
 	if size > 64*MIB || size < 1 {
--- src/bytesGenerator_test.go
+++ src/bytesGenerator_test.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"encoding/base64"
+	"io/ioutil"
+
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 )
@@ -14,5 +17,16 @@ var _ = Describe("The bytes generator", func() {
 		Expect(err).Should(HaveOccurred())
 		err = validateBlobSize(1)
 		Expect(err).Should(Succeed())
+	})
+
+	Specify("Should generate exactly the requested size", func() {
+		r, err := getBlob(10)
+		Expect(err).Should(Succeed())
+		rb64 := base64.NewDecoder(base64.StdEncoding, r)
+		data, err := ioutil.ReadAll(rb64)
+
+		Expect(err).Should(Succeed())
+
+		Expect(len(data)).Should(Equal(10))
 	})
 })
